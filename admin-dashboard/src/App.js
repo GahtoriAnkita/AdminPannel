05f3ae436a976dedc867cbb3d596982f279950b7
GahtoriAnkita <gahtoriankita03@gmail.com>
2024-02-11 12:31:24 +0530

    header

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,10 +1,17 @@
-
+import React from 'react'
 import './App.css';
+import Header from "./components/Header"
+//Import Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'remixicon/fonts/remixicon.css'
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 
 function App() {
-  return (
-  <h1>Hello</h1>
-  );
+  return <Header />
 }
 
 export default App;
