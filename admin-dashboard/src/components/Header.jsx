4fbf9ad67cbd4cdc16c5e13e10fb0a969992a3a6
GahtoriAnkita <gahtoriankita03@gmail.com>
2024-02-11 12:50:59 +0530

    navbar created

--- admin-dashboard/src/components/Header.jsx
+++ admin-dashboard/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './header.css'
 import Logo from "./Logo"
+import SearchBar from "./SearchBar"
+import Nav from "./Nav"
 function Header() {
     return (
         <header id="header" className='header fixed-top d-flex align-items-center'>
-        {/*{logo}*/} 
-        <Logo/>
-        {/*{search bar}*/} 
-       {/* {nav}*/} 
+            {/*{logo}*/}
+            <Logo />
+            {/*{search bar}*/}
+            <SearchBar />
+            {/* {nav}*/}
+            <Nav />
         </header>
     )
 }
---
+++ admin-dashboard/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./nav.css"
+function Nav() {
+  return (
+    <div>
+      nav
+    </div>
+  )
+}
+
+export default Nav
