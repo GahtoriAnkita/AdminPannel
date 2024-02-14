0c495651a28fb5301e27afe286f2e81e8f2a8260
GahtoriAnkita <gahtoriankita03@gmail.com>
2024-02-14 19:48:49 +0530

    setting dashboard

---
+++ admin-dashboard/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card(card) {
+    return (
+        <>
+            <div>{card.name}</div>
+        </>
+
+    )
+}
+
+export default Card
--- admin-dashboard/src/components/Dashboard.jsx
+++ admin-dashboard/src/components/Dashboard.jsx
@@ -1,13 +1,33 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "./dashboard.css"
+import Card from './Card';
 function Dashboard() {
+    const { cards, setCards } = useState([])
+
+    const fetchData = () => {
+        fetch('http://localhost:4000/cards')
+            .then(res => res.json())
+            .then(data => {
+                setCards(data);
+            })
+            .catch(e => console.log(e.message));
+    };
+
+    useEffect(() => {
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <>
             <section className="dashboard section">
                 <div className="row">
                     <div className="col-lg-8">
                         <div className="row">
-
+                            {
+                                cards && 
+                                cards.length>0 && 
+                                cards.map(card => <Card key={card._id} card={card}/>)
+                            }
                         </div>
                     </div>
                     <div className="col-lg-4">
